dddde046966f79f0c70c4a2d00762ec566ecb9b8
naufalafles <zamrinaufalcode@gmail.com>
2025-12-12 02:36:36 +1100

    Initialising and Layout for Login Page
    Initialising and Layout for Login Page - Could use some refinements.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './components/Login';
+import Login from './pages/Login';
 
 const App = () => {
   return (

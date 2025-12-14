91f342aeb041fcae0daff9f7627c00964e082e32
naufalafles <zamrinaufalcode@gmail.com>
2025-12-14 19:34:49 +1100

    Initialising Car Router Backend Functions
    Initialising and Layingout to test the car router backend functions, MONGODB

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,47 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+import path from 'path';
+import helmet from 'helmet';
+import { fileURLToPath } from 'url';
+
 import { connectDB } from './config/db.js';
 import userRouter from './routes/userRoutes.js';
+import carRouter from './routes/carRoutes.js';
 
 
 const app = express();
 const PORT = 7889;
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 connectDB();
 
 // MIDDLEWARES
 app.use(cors());
+app.use(
+    helmet({
+        crossOriginResourcePolicy: { policy: 'cross-origin' },
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    '/uploads', (req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', "*");
+        next();
+    },
+    express.static(path.join(process.cwd(), 'uploads'))
+)
 
 
 // ROUTES
 app.use('/api/auth', userRouter);
+app.use('/api/cars', carRouter);
 
 app.get('api/ping', (req, res) => res.json({
     ok: true,

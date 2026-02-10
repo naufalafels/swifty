7a8737345e56a25921f8c41794b00a62ec1ade74
naufalafles <zamrinaufalcode@gmail.com>
2026-02-10 18:50:33 +0800

    Update ProfilePage for S3

--- backend/services/s3Service.js
+++ backend/services/s3Service.js
@@ -2,11 +2,6 @@ import { S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 
-// DEBUG: Check env vars
-console.log('S3Service: AWS_REGION =', process.env.AWS_REGION);
-console.log('S3Service: AWS_ACCESS_KEY_ID set =', !!process.env.AWS_ACCESS_KEY_ID);
-console.log('S3Service: AWS_SECRET_ACCESS_KEY set =', !!process.env.AWS_SECRET_ACCESS_KEY);
-
 const s3Client = new S3Client({
   region: 'ap-southeast-1',
 });
@@ -24,7 +19,7 @@ export async function generateUploadUrl(key, contentType, expiresIn = 900) {
   const command = new PutObjectCommand({
     Bucket: BUCKET_NAME,
     Key: key,
-    ContentType: contentType,
+    // ContentType removed to avoid signed header mismatch
   });
   return await getSignedUrl(s3Client, command, { expiresIn });
 }
